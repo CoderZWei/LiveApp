9c165ae1d4a5374381fe1604c547edb08b614276
zw <1622993630@qq.com>
2019-08-16 15:28:21 +0800

    集成RTMP

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,7 +1,12 @@
 #include <jni.h>
 #include <string>
 
+extern "C"{
+    #include "librtmp/rtmp.h"
+}
 extern "C" JNIEXPORT jstring JNICALL
+
+
 Java_com_example_zw_liveapp_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
